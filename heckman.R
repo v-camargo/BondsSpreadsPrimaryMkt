4a39c0197e5920f29bf58ba4907e35ab676eb291
Vinicius Camargo <vinicius.camargo@augme.local>
2023-04-25 18:02:43 -0300

    adjusted data

---
+++ heckman.R
@@ -0,0 +1,27 @@
+### Bibliotecas Importadas -----------------------------------------------------
+
+library(tidyverse)
+
+### Tratamento dos Dados -------------------------------------------------------
+
+base2 <- readxl::read_xlsx("Base2PreliminarTratada.xlsx")
+
+heckit <- base2 |> select(`Empresa (j)`, βLP, βExtAccess, `Volume R$ MM`, βHighGrade, 
+                `Volume Médio Mercado Bonds R$ MM`,`LP Mercado Bonds (>5 anos)`,
+                `Volume Médio Debs R$ MM`,`LP Debs (>5 anos)`)
+heckit <- heckit |> rename(empresaId=1,
+                 BLP=2,
+                 BExtAcess=3,
+                 volEmissao=4,
+                 BHG=5,
+                 volMedBonds=6,
+                 percLPAnoBonds=7,
+                 volMedDebs=8,
+                 percLPAnoDebs=9
+                 )
+heckit <- heckit |> mutate(empresaId = empresaId |> as.factor(),
+                 BLP = BLP |> as.factor(),
+                 BExtAcess = BExtAcess |> as.factor(),
+                 BHG = BHG |> as.factor()
+                 )
+
